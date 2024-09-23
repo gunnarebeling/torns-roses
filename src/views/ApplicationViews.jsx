9cd2366568aeb95b21e6f02e5856c9a768d195b3
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-09-23 11:32:00 -0500

    added margin to nav links

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -10,7 +10,7 @@ export const ApplicationViews = () => {
                 path="/"
                 element={
                     <>
-                        {/* <NavBar/> */}
+                        <NavBar/>
                         <Outlet/>
                     </>
                 }>
