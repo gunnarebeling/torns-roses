79d6c36eff150d924a440f6321c0dcfa0e896388
Taylor Albright <jtayloralbright@gmail.com>
2024-09-23 11:16:49 -0500

    Added flowers link

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -12,6 +12,9 @@ export const NavBar = () => {
             <li className='nav-item'>
                 <Link className='nav-link' to='/retailers'>Retailers</Link>
             </li>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/flowers'>Flowers</Link>
+            </li>
         </ul>
     )
 }
